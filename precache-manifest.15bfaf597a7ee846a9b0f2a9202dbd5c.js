4327fe20965d49301d40113c7cbba07869e20224
Muzammil Ahmed <engr.muzammil95@gmail.com>
2020-06-09 15:34:12 +0500

    Updates

---
+++ precache-manifest.15bfaf597a7ee846a9b0f2a9202dbd5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d4520eabf351d76d76ded9f422ddb2",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "2839a3b5a3221f5281d8",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/React-Deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2839a3b5a3221f5281d8",
+    "url": "/React-Deploy/static/js/main.8bc6ee05.chunk.js"
+  },
+  {
+    "revision": "a525b113631449d64929",
+    "url": "/React-Deploy/static/js/runtime-main.78586af6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
